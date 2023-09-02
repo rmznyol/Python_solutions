de3febd174af8f0420b737782202e00ba25915af
Ramazan Yol <rmznyol@gmail.com>
2023-09-02 11:08:58 -0700

    add LeetCode75 solutions for Heap/Priority Queue section

--- LeetCode75_Linked_List.py
+++ LeetCode75_Linked_List.py
@@ -4,8 +4,7 @@ class ListNode:
         self.val = val
         self.next = next
 
-# 1721. Swapping Nodes in a Linked List
-
+# 1721. Swapping Nodes in a Linked List (EXTRA)
 class Solution:
     def swapNodes(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
         temp = head
@@ -20,7 +19,6 @@ class Solution:
         return head
 
 ##############################################################################################################
-
 # 2095. Delete the Middle Node of a Linked List
 
 class Solution:
@@ -34,4 +32,47 @@ class Solution:
             slow = slow.next if slow else head
         slow.next = slow.next.next
         return head
+##############################################################################################################
+# 328. Odd Even Linked List
+
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         
+        if head == None or head.next == None:
+            return head 
+        temp_odd = head
+        temp_even = head.next
+        middle_head = head.next
+        i = 1 
+        temp = head.next.next
+        while temp:
+            if i % 2 != 0:
+                temp_odd.next = temp
+                temp_odd = temp_odd.next
+
+            else:
+                temp_even.next = temp
+                temp_even = temp_even.next
+            i += 1
+            temp = temp.next
+        temp_even.next = None
+        temp_odd.next = middle_head
+
+        return head
+##############################################################################################################
+# 206. Reverse Linked List
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head == None:
+            return head
+        before = None
+        after = head
+        while after!= None:
+            temp = after
+            after = after.next
+            temp.next = before
+            before = temp
+        return before
+##############################################################################################################
+# 2130. Maximum Twin Sum of a Linked List    
