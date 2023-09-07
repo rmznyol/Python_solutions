3c37cc02dcd68878aed503fe7699e0c298aab592
Ramazan Yol <rmznyol@gmail.com>
2023-09-07 12:03:06 -0700

    add solution for #198

--- LeetCode75_DP_1D.py
+++ LeetCode75_DP_1D.py
@@ -31,5 +31,23 @@ class Solution:
             cost_from = [cost_from_current_cost,cost_from[0]]
         return min(cost_from)
 ######################################################################
+#198. House Robber
 
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        dp = [0,0]+[nums[0] for _ in nums]
+        last_house_is_robbed = True
+        for i, current_house in enumerate(nums[1:],3):
+            if not last_house_is_robbed:
+                dp[i] = dp[i - 1] + current_house
+                last_house_is_robbed = True
+            elif dp[i - 2] + current_house > dp[i-1]:
+                dp[i] = dp[i - 2] + current_house
+                last_house_is_robbed = True
+            else:
+                dp[i] = dp[i-1]
+                last_house_is_robbed = False
+        return dp[-1]
 ######################################################################
+# 790. Domino and Tromino Tiling
+
