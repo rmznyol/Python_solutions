3397509449b7e4200283edd9c764b048a93b7f76
Ramazan Yol <rmznyol@gmail.com>
2023-09-08 13:47:45 -0700

    add solution for #994

--- LeetCode75_Graphs_BFS.py
+++ LeetCode75_Graphs_BFS.py
@@ -20,3 +20,43 @@ class Solution:
                     queue.append([ni, nj, d + 1])
 
         return -1
+####################################################################################
+# 994. Rotting Oranges
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+        def is_safe(i,j):
+            return 0 <= i < rows and 0 <= j < cols
+        directions = ((1,0),(-1,0),(0,1),(0,-1))
+        # add all the bad oranges to queue and see if there are any oranges to begin with
+        bad_oranges = collections.deque()
+        orange_count = 0
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == 2:
+                    bad_oranges.append((i,j,0))
+                elif grid[i][j] == 1:
+                    neighbors = [(i+di, j+dj) for di, dj in directions if is_safe(i+di,j+dj)] 
+                    no_access = [(i,j) for i,j in neighbors if grid[i][j] == 0]
+                    if len(no_access) == len(neighbors):
+                        # if there are inaccessible oranges, return -1 immediately
+                        return -1   
+                if grid[i][j] != 0: orange_count += 1
+        # no oranges?, return 0
+        if orange_count == 0: return 0
+        max_time = -1
+        for row in grid:
+            print(row)
+        while bad_oranges:
+            i, j, t = bad_oranges.popleft()
+            max_time = max(t,max_time)
+            for di, dj in directions:
+                ni, nj = i + di, j + dj
+                if is_safe(ni,nj) and grid[ni][nj] == 1:
+                     grid[ni][nj] = 2
+                     bad_oranges.append((ni,nj,t+1))
+        for row in grid:
+            for cell in row:
+                if cell == 1:  # still got a fresh orange left
+                    return -1
+        return max_time
