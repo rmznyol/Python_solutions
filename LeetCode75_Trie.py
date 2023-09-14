591983c72f293a0de11308d57c84df60b65cd73d
Ramazan Yol <rmznyol@gmail.com>
2023-09-14 13:19:29 -0700

    add solution for #1268

--- LeetCode75_Trie.py
+++ LeetCode75_Trie.py
@@ -1,3 +1,5 @@
+# 208. Implement Trie (Prefix Tree)
+
 class TrieNode:
     def __init__(self, val) -> None:
         # example children value: Node for quick checkups
@@ -38,3 +40,20 @@ class Trie:
             else:
                 return False
         return False
+########################################################################
+# 1268. Search Suggestions System
+
+class Solution:
+    def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
+        #important point is that list is getting shorter, no need to search through the products all over again every time? That is not true 
+        l=len(searchWord)
+        results=[]
+        products.sort()
+        for i in range(l):
+            temp=[]
+            for word in products:
+                if searchWord[0:i+1]==word[0:i+1]:
+                    temp.append(word)
+            products=temp
+            results.append(products[:min(3,len(products))])
+        return results
