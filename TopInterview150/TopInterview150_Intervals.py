735e8fadb3f668b33afff01f05ac660954b1d23c
Ramazan Yol <rmznyol@gmail.com>
2023-09-23 14:24:35 -0700

    add solution for #452

--- TopInterview150/TopInterview150_Intervals.py
+++ TopInterview150/TopInterview150_Intervals.py
@@ -47,3 +47,17 @@ class Solution:
                 curr += 1
         return intervals
 #################################################
+# 452. Minimum Number of Arrows to Burst Balloons
+
+class Solution:
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+        points.sort(key=lambda x: x[0])
+        end_point = points[0][1]
+        count = 0
+        for point in points[1:]:
+            if point[0] <= end_point: #intersects
+                end_point = min(point[1],end_point)
+                count += 1
+            else:
+                end_point = point[1]
+        return len(points) - count
