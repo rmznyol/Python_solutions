29d5e8f50bd4882a35d38e579bba239d06dedc4c
Ramazan Yol <rmznyol@gmail.com>
2023-10-03 16:38:15 -0700

    add solution for #211

--- TopInterview150/TopInterview150_Trie.py
+++ TopInterview150/TopInterview150_Trie.py
@@ -40,3 +40,44 @@ class Trie:
 # obj.insert(word)
 # param_2 = obj.search(word)
 # param_3 = obj.startsWith(prefix)
+###############################################################
+#211. Design Add and Search Words Data Structure
+class TrieNode:
+    def __init__(self, val) -> None:
+        # example children value: Node for quick checkups
+        self.val = val
+        self.children = {}
+        self.is_end = False
+
+class WordDictionary:
+
+    def __init__(self):
+        self.children = {}
+
+    def addWord(self, word: str) -> None:
+        curr = self
+        for i, char in enumerate(word):
+            if char not in curr.children:
+                curr.children[char] = TrieNode(char)
+            curr = curr.children[char]
+            curr.is_end = True if i == len(word) - 1 else curr.is_end
+
+    def search(self, word: str,curr = None) -> bool:
+        curr = curr if curr else self
+        if len(word) == 1:
+            if word[0] == '.':
+                for child in curr.children:
+                    if curr.children[child].is_end:
+                        return True
+            elif word[0] in curr.children and curr.children[word[0]].is_end:
+                return True
+            return False
+        if word[0] == '.':
+            for child in curr.children:
+                if self.search(word[1:],curr.children[child]):
+                    return True
+        if word[0] in curr.children:
+            return self.search(word[1:],curr.children[word[0]])
+        else:
+            return False
+#################################################
