b22df9f36334d4375dbc9833e35df22bf32ead05
Ramazan Yol <rmznyol@gmail.com>
2023-10-02 18:56:55 -0700

    add solution for #918

--- TopInterview150/TopInterview150_Kadanes_Algorithm.py
+++ TopInterview150/TopInterview150_Kadanes_Algorithm.py
@@ -10,3 +10,15 @@ class Solution:
         return global_max    
 
 #########################################
+# 918. Maximum Sum Circular Subarray
+
+class Solution:
+    def maxSubarraySumCircular(self, nums: List[int]) -> int:
+        total = global_max = max_ending = global_min = min_ending = nums[0]
+        for num in nums[1:]:
+            max_ending = max(num, num + max_ending)
+            min_ending = min(num, num + min_ending)
+            global_max = max(max_ending,global_max)
+            global_min = min(min_ending,global_min)
+            total += num
+        return max(global_max, total - global_min) if total != global_min else global_max
