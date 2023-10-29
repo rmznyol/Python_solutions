48093975dedbecaab4944f930520204383303a19
Ramazan Yol <rmznyol@gmail.com>
2023-10-28 22:40:20 -0700

    add solution for #205

--- TopInterview150/TopInterview150_Hashmap.py
+++ TopInterview150/TopInterview150_Hashmap.py
@@ -10,3 +10,26 @@ class Solution:
             else:
                 return False
         return True
+##########################################
+
+# 205. Isomorphic Strings
+
+from collections import Counter
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        s_to_t = {}
+        t_to_s = {}
+        sl = len(s)
+        tl = len(t)
+        if sl == tl:
+            for i in range(sl):
+                if s_to_t.get(s[i]) == None:
+                    s_to_t[s[i]] = t[i]
+                elif s_to_t[s[i]] != t[i]:
+                    return False
+                if t_to_s.get(t[i]) == None:
+                    t_to_s[t[i]] = s[i]
+                elif t_to_s[t[i]] != s[i]:
+                    return False
+            else:
+                return True
