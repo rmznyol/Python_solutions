8cf4876a0b5ac9db998098e5c33d33b90e645e86
Ramazan Yol <rmznyol@gmail.com>
2023-11-06 21:06:29 -0800

    add solution for #2

--- TopInterview150/TopInterview150_Linked_List.py
+++ TopInterview150/TopInterview150_Linked_List.py
@@ -9,3 +9,32 @@ class Solution:
             record[temp] = True
             temp = temp.next
 ############################################################
+# 2. Add Two Numbers
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        # Definition for singly-linked list.
+        a=ListNode((l1.val+l2.val)%10)
+        temp=a
+        carry=(l1.val+l2.val)//10
+        l1,l2=l1.next,l2.next
+        while (l1 is not None) and (l2 is not None):
+            temp.next=ListNode((l1.val+l2.val+carry)%10)
+            carry=(l1.val+l2.val+carry)//10
+            l1,l2=l1.next,l2.next
+            temp=temp.next
+        while (l1 is not None):
+            temp.next=ListNode((l1.val+carry)%10)
+            carry=(l1.val+carry)//10
+            l1=l1.next
+            temp=temp.next
+        while (l2 is not None):
+            temp.next=ListNode((l2.val+carry)%10)
+            carry=(l2.val+carry)//10
+            l2=l2.next
+            temp=temp.next
+        if carry>0:
+            temp.next=ListNode(carry)
+        return a
+
+############################################################
