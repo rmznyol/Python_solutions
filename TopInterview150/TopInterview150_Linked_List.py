c8a87e2f446a2a8140221645878fa569cea02761
Ramazan Yol <rmznyol@gmail.com>
2023-11-08 19:42:12 -0800

    add solution for #21

--- TopInterview150/TopInterview150_Linked_List.py
+++ TopInterview150/TopInterview150_Linked_List.py
@@ -38,3 +38,25 @@ class Solution:
         return a
 
 ############################################################
+# 21. Merge Two Sorted Lists
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        p1 = list1
+        p2 = list2
+        p3 = head = ListNode()
+        while p1 and p2:
+            if p1.val < p2.val:
+                p3.next = p1
+                p3 = p1
+                p1 = p1.next
+            else:
+                p3.next = p2
+                p3 = p2
+                p2 = p2.next
+        if p1 is None:
+            p3.next = p2
+        else:
+            p3.next = p1 
+        return head.next 
+    
+############################################################
